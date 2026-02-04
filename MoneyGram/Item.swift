ab0fa6fee2171a16269665f2a8dd37134df66ec6
Viktor Parshyn <viktorparshin8@gmail.com>
2026-02-04 19:29:11 +0100

    Initial Commit

---
+++ MoneyGram/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MoneyGram
+//
+//  Created by Viktor Parshyn on 2026-02-04.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ MoneyGramTests/MoneyGramTests.swift
@@ -0,0 +1,16 @@
+//
+//  MoneyGramTests.swift
+//  MoneyGramTests
+//
+//  Created by Viktor Parshyn on 2026-02-04.
+//
+
+import Testing
+
+struct MoneyGramTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
